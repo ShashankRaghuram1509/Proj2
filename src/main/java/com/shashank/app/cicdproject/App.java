4e28e8686ddd21929fc0aaca1207af14daf483ed
ShashankRaghuram1509 <shashankr829@gmail.com>
2025-06-03 08:11:05 +0530

    first commit

---
+++ src/main/java/com/shashank/app/cicdproject/App.java
@@ -0,0 +1,10 @@
+package com.shashank.app.cicdproject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
